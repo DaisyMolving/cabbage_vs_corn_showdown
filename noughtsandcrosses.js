f4e575d32c2b27ed80b692b2b5ce3c2be83311b7
Daisy Molving <daisymolving@gmail.com>
2015-05-13 03:11:51 +1200

    linked the grid inputs to the click function

--- noughtsandcrosses.js
+++ noughtsandcrosses.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
 
 var turn = true;
-var board = ["","","","","","","","",""]; {
-  $("#one") = board[0]
-  
-}
+
 
 $(".gridBox").on('click', function(){
     if (turn === true) {
@@ -15,6 +12,39 @@ $(".gridBox").on('click', function(){
       turn = true;
     }
 })
+
+var board = ["0","1","2","3","4","5","6","7","8"]; 
+
+
+
+  $("#one").on('click', function(){
+    console.log(board[0]);
+  }); 
+  $("#two").on('click', function(){
+    console.log(board[1]);
+  });
+  $("#three").on('click', function(){
+    console.log(board[2]);
+  });
+  $("#four").on('click', function(){
+    console.log(board[3]);
+  });  
+  $("#five").on('click', function(){
+    console.log(board[4]);
+  });
+  $("#six").on('click', function(){
+    console.log(board[5]);
+  });
+  $("#seven").on('click', function(){
+    console.log(board[6]);
+  });
+  $("#eight").on('click', function(){
+    console.log(board[7]);
+  });
+  $("#nine").on('click', function(){
+    console.log(board[8]);
+  });
+
 
 
 
