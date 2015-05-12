deead3f1bbd498ccfcb819243c2eabb72cce26ca
Daisy Molving <daisymolving@gmail.com>
2015-05-13 01:45:55 +1200

    made inputs for player one and player two to choose x or o

--- noughtsandcrosses.js
+++ noughtsandcrosses.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 
-$(".gridBox").on('click', function(){
-    $(this).css("background-color", "red");
-})
+// var playerTwo = 0;
+var playerOne = function(){
+  prompt("Would you like to be noughts or crosses? (The computer will randomly select who starts)");
+   if (playerOne === "crosses") {
+    console.log("crosses");
+   } else if (playerOne === "noughts") {
+    console.log("noughts");
+   }
+};
+
 
 var xTurn
 var oTurn
@@ -11,5 +18,9 @@ var board = [[1,2,3],[4,5,6],[7,8,9]];
 
 var cross = -1;
 var nought = 1;
+
+$(".gridBox").on('click', function(){
+    $(this).css("background-color", "red");
+})
 
 });
