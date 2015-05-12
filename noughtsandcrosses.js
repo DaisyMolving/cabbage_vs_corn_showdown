16e899239627ce8bcc3c39c08238e4b187a2fb27
Daisy Molving <daisymolving@gmail.com>
2015-05-12 22:26:50 +1200

    created variables of empty arrays for all eight line possibilities. create variables for nought and cross that are 1 and -1 respectively

--- noughtsandcrosses.js
+++ noughtsandcrosses.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+var diagonalOne = [];
+var diagonalTwo = [];
+var verticalLeft = [];
+var verticalMiddle = [];
+var verticalRight = [];
+var horizontalTop = [];
+var horizontalMiddle = [];
+var horizontalBottom = [];
+
+var cross = -1;
+var nought = 1;
