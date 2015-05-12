2c91a3a09d142777179aa6fb6ea4d1422e6dd274
Daisy Molving <daisymolving@gmail.com>
2015-05-12 23:00:32 +1200

    changed the eight arrays of possible win lines into an array of arrays with numbers as place holders for the positions

--- noughtsandcrosses.js
+++ noughtsandcrosses.js
@@ -1,13 +1,9 @@
-console.log("hello");
+console.log();
 
-var diagonalOne = [];
-var diagonalTwo = [];
-var verticalLeft = [];
-var verticalMiddle = [];
-var verticalRight = [];
-var horizontalTop = [];
-var horizontalMiddle = [];
-var horizontalBottom = [];
+var xTurn
+var oTurn
+
+var board = [[1,2,3],[4,5,6],[7,8,9]];
 
 var cross = -1;
 var nought = 1;
