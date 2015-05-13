9be7d373c9007e29ac7a6b7edbd0c9551abbc8d3
Daisy Molving <daisymolving@gmail.com>
2015-05-14 07:46:11 +1200

    digi-painted pixel pics for background and grid, changed background, added title and subtitled, positioned grid, made getWinner function work, moved array of winning arrays inside it

--- noughtsandcrosses.js
+++ noughtsandcrosses.js
@@ -5,10 +5,10 @@ var turn = -1;
 
 $(".gridBox").on('click', function(){
     if (turn === -1) {
-      $(this).css("background-image", "url(O.png)");
+      $(this).css("background-image", "url(cabbage.png)");
       turn = 1; 
     } else {
-      $(this).css("background-image", "url(X.png)");
+      $(this).css("background-image", "url(corn.png)");
       turn = -1;
     }
 })
@@ -70,7 +70,14 @@ var gridPosition = [0,0,0,0,0,0,0,0,0];
 // diagonalLeftTop = [gridPosition[0], gridPosition[4], gridPosition[8]];
 // diagonalRightTop = [gridPosition[2], gridPosition[4], gridPosition[6]];
 
-var threeInRow = [[gridPosition[0], gridPosition[1], gridPosition[2]], 
+
+
+// threeInRow[0].reduce(function(a, b) {
+//     return parseInt(a, 10) + parseInt(b, 10);
+// })
+
+function winnerTest(){
+  var threeInRow = [[gridPosition[0], gridPosition[1], gridPosition[2]], 
                   [gridPosition[3], gridPosition[4], gridPosition[5]], 
                   [gridPosition[6], gridPosition[7], gridPosition[8]], 
                   [gridPosition[0], gridPosition[3], gridPosition[6]], 
@@ -78,22 +85,23 @@ var threeInRow = [[gridPosition[0], gridPosition[1], gridPosition[2]],
                   [gridPosition[2], gridPosition[5], gridPosition[8]], 
                   [gridPosition[0], gridPosition[4], gridPosition[8]], 
                   [gridPosition[2], gridPosition[4], gridPosition[6]]];
+  threeInRow.forEach(function(element, i){
+  
+    console.log(element);
+     var sum = element.reduce(function(a, b) {
+        console.log(a+b);
+        return (a+b);
+    });
 
-// threeInRow[0].reduce(function(a, b) {
-//     return parseInt(a, 10) + parseInt(b, 10);
-// })
-
-function winnerTest(){
-threeInRow.some(function(element, i){
-  console.log(element);
-   var sum = threeInRow[i].reduce(function(a, b) {
-      console.log(a+b);
-      return (a+b);
-  });
-   //console.log(sum);
-   // this.val() === -3)) { 
-    // console.log("x wins");
-}); 
+     if (sum === 3){
+      alert("CABBAGE WINS! Corn, you got cole-slawtered");
+     } else if (sum === -3){
+      alert("CORN WINS! Cabbage, looks like you are going through a bad patch");
+     }
+     //console.log(sum);
+     // this.val() === -3)) { 
+      // console.log("x wins");
+  }); 
 }
 
   // else if (threeInRow === 3) {
