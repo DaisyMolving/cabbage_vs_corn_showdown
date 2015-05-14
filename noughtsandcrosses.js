9885b70b0c452bd8f9fe572701b05644d3b3f5be
Daisy Molving <daisymolving@gmail.com>
2015-05-15 01:26:48 +1200

    created variable for number of turns whereby when a button is clicked it adds one to the variable. when there are nine turns and no winner it is a draw. tweeked css positioning. made jQuery pop=ups for winner/draw

--- noughtsandcrosses.js
+++ noughtsandcrosses.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 
+// var openDialogCabbage = $( ".cabbageWins" ).dialog( "option", "modal" );
+
+
+function openDialogCabbage(){
+  $( ".cabbageWins" ).dialog();
+}
+
+// var openDialogCorn = $( ".cornWins" ).dialog( "option", "modal" );
+
+function openDialogCorn(){
+  $( ".cornWins" ).dialog();
+}
+
 var turn = -1;
+var numberOfTurns = 0;
 
 
 $(".gridBox").on('click', function(){
@@ -17,59 +31,68 @@ var gridPosition = ["","","","","","","","",""];
 
   $("#one").on('click', function(){
     gridPosition[0] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   }); 
   $("#two").on('click', function(){
     gridPosition[1] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });
   $("#three").on('click', function(){
     gridPosition[2] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });
   $("#four").on('click', function(){
     gridPosition[3] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });  
   $("#five").on('click', function(){
     gridPosition[4] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });
   $("#six").on('click', function(){
     gridPosition[5] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });
   $("#seven").on('click', function(){
     gridPosition[6] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });
   $("#eight").on('click', function(){
     gridPosition[7] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });
   $("#nine").on('click', function(){
     gridPosition[8] = turn;
+    numberOfTurns+=1;
     console.log(gridPosition);
     winnerTest();
   });
 
-function drawTest() {
-  gridPosition(function(element, i){
-     var sumOfAll = element.reduce(function(a, b) {
-        console.log(sumOfAll)
-        return (a+b);
-    });
+// function drawTest() {
+//   gridPosition(function(element, i){
+//      var sumOfAll = element.reduce(function(a, b) {
+//         console.log(sumOfAll)
+//         return (a+b);
+//     });
 
-});
-}
+// });
+// }
 
 
 // horizontalTop = [gridPosition[0], gridPosition[1], gridPosition[2]];
@@ -92,6 +115,7 @@ function drawTest() {
 // }
 
 function winnerTest(){
+  var sum;
   var threeInRow = [[gridPosition[0], gridPosition[1], gridPosition[2]], 
                   [gridPosition[3], gridPosition[4], gridPosition[5]], 
                   [gridPosition[6], gridPosition[7], gridPosition[8]], 
@@ -100,24 +124,32 @@ function winnerTest(){
                   [gridPosition[2], gridPosition[5], gridPosition[8]], 
                   [gridPosition[0], gridPosition[4], gridPosition[8]], 
                   [gridPosition[2], gridPosition[4], gridPosition[6]]];
+ 
   threeInRow.forEach(function(element, i){
-     var sum = element.reduce(function(a, b) {
-        console.log(a+b);
+    console.log(element);
+     sum = element.reduce(function(a, b) {
         return (a+b);
     });
 
+// function drawTest() {
+//   gridPosition(function(element, i){
+//      var sumOfAll = element.reduce(function(a, b) {
+//         console.log(sumOfAll)
+//         return (a+b);
+//      }
+//      });   
      if (sum === 3){
-      alert("CABBAGE WINS! Corn, you got cole-slawtered")
+     openDialogCabbage();
      } else if (sum === -3){
-      alert("CORN WINS! Cabbage, looks like you are going through a bad patch")
-     } else if (sumOfAll === 1) {
+       openDialogCorn();
+     } 
+
+  }); 
+  if (numberOfTurns === 9 && sum !== 3 && sum !== -3) {
       alert("nobody has won");
      }
-     //console.log(sum);
-     // this.val() === -3)) { 
-      // console.log("x wins");
-  }); 
 }
+
 
   // else if (threeInRow === 3) {
   //   console.log("o wins");
